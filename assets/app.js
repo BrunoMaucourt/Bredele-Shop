de2a69ccd1778eed5e709818b3c3014d6994b543
Bruno <mct.bruno@gmail.com>
2023-05-02 11:04:52 +0200

    header and back - front for smartphone

--- assets/app.js
+++ assets/app.js
@@ -9,4 +9,31 @@
 import './styles/app.css';
 
 // start the Stimulus application
-import './bootstrap';
+// import './bootstrap';
+
+// Modify header position in function of scroll
+const header = document.getElementById("header");
+const header_space = document.getElementById("header_space");
+let lastKnownScrollPosition = 0;
+
+document.addEventListener("scroll", function () {
+    let newKnownScrollPosition = window.scrollY;
+    if (newKnownScrollPosition < lastKnownScrollPosition && newKnownScrollPosition > 500) {
+        header.classList.remove("absolute");
+        header.classList.add("fixed");
+    } else {
+        header.classList.add("absolute");
+        header.classList.remove("fixed");
+    }
+    // Update position
+    lastKnownScrollPosition = newKnownScrollPosition;
+})
+
+// Display and hidde footer menu when click
+const footer_responsive_menu = document.querySelectorAll(".footer-responsive-menu");
+footer_responsive_menu.forEach((menu) => {
+    menu.addEventListener("click", function () {
+        let menu_next_element = menu.nextElementSibling;
+        menu_next_element.classList.toggle("hidden");
+    })
+})
